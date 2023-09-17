9d8c949a8d5a9b6857eeb39f316456d5459573be
Yuri Vishnevsky <yurivish@gmail.com>
2023-09-16 20:08:20 -0400

    add live-watching build script

---
+++ build.js
@@ -0,0 +1,37 @@
+import * as esbuild from 'esbuild';
+
+const plural = n => n === 1 ? '' : 's';
+
+let examplePlugin = {
+  name: 'example',
+  setup(build) {
+    build.onEnd(result => {
+      console.log(`build ended with ${result.errors.length} error${plural(result.errors.length)}`);
+    });
+  },
+};
+
+const ctx = await esbuild.context({
+  entryPoints: ['./src/index.js'],
+  bundle: true,
+  format: 'esm',
+  plugins: [examplePlugin],
+  outfile: 'dist/bit-structures.js', 
+  sourcemap: 'inline'
+});
+
+await ctx.watch();
+console.log('watching...');
+
+const { host, port } = await ctx.serve({
+  servedir: 'dist', 
+  port: 4242,
+  // SSL certificates for local serving generated using the following command,
+  // which was taken from the esbuild documentation: https://esbuild.github.io/api/#https
+  //
+  // openssl req -x509 -newkey rsa:4096 -keyout silk.key -out silk.cert -days 9999 -nodes -subj /CN=127.0.0.1
+  keyfile: 'silk.key',
+  certfile: 'silk.cert',
+});
+
+console.log(`serving on https://${host}:${port}`);
