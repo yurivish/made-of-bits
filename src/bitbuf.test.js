aa468ba2084020e86ca4d09d585f3f0e81cd70b1
Yuri Vishnevsky <yurivish@gmail.com>
2023-09-17 23:22:16 -0400

    update tests

--- src/bitbuf.test.js
+++ src/bitbuf.test.js
@@ -6,26 +6,26 @@ describe('BitBuf', () => {
 
   it('should be initialized to false', () => {
 
-    expect(xs.get(0)).toBe(false);
-    expect(xs.get(1)).toBe(false);
-    expect(xs.get(2)).toBe(false);
+    expect(xs.get(0)).toBe(0);
+    expect(xs.get(1)).toBe(0);
+    expect(xs.get(2)).toBe(0);
   });
 
   it('should set bits individually', () => {
     xs.setOne(1);
-    expect(xs.get(0)).toBe(false);
-    expect(xs.get(1)).toBe(true);
-    expect(xs.get(2)).toBe(false);
+    expect(xs.get(0)).toBe(0);
+    expect(xs.get(1)).toBe(1);
+    expect(xs.get(2)).toBe(0);
 
     xs.setOne(2);
-    expect(xs.get(0)).toBe(false);
-    expect(xs.get(1)).toBe(true);
-    expect(xs.get(2)).toBe(true);
+    expect(xs.get(0)).toBe(0);
+    expect(xs.get(1)).toBe(1);
+    expect(xs.get(2)).toBe(1);
 
     xs.setOne(0);
-    expect(xs.get(0)).toBe(true);
-    expect(xs.get(1)).toBe(true);
-    expect(xs.get(2)).toBe(true);
+    expect(xs.get(0)).toBe(1);
+    expect(xs.get(1)).toBe(1);
+    expect(xs.get(2)).toBe(1);
   });
 
   it('should throw errors if the index is out of bounds.', () => {
