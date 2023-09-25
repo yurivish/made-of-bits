02cbe87eb98ce9c0342be9fa1b92aaa595f65bd5
Yuri Vishnevsky <yurivish@gmail.com>
2023-09-24 21:16:02 -0400

    tweak the debug/globalThis setup

--- src/debug.js
+++ src/debug.js
@@ -1,6 +1,5 @@
 // @ts-ignore
-// The `globalThis &&` allows us to substitute `globalThis` with `false`, and
 // ESBuild's constant propagation will remove this if statement at build time
-if (globalThis && globalThis.DEBUG === undefined) {
+globalThis && !('DEBUG' in globalThis) &&
   Object.defineProperty(globalThis, 'DEBUG', { value: true });
-}
+
