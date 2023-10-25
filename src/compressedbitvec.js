09e81acd3f5269290b4619ec9ef437427c6abb8d
Yuri Vishnevsky <yurivish@gmail.com>
2023-10-24 22:30:59 -0400

    add empty compressed bitvec as a to-do

---
+++ src/compressedbitvec.js
@@ -0,0 +1,9 @@
+// todo:
+// Compressed bit vector with rank and select, based on the ideas described in the paper
+// 
+//   - Title: Fast, Small, Simple Rank/Select on Bitmaps
+//   - Authors: Gonzalo Navarro and Eliana Providel
+//   - Link: https://users.dcc.uchile.cl/~gnavarro/ps/sea12.1.pdf
+//
+// prototype of the core encode/decode feature:
+// https://observablehq.com/d/5370347688e58b4d
