1cce32817d22f9681a8a0a15b9a77412189b4484
Yuri Vishnevsky <yurivish@gmail.com>
2024-07-29 17:51:04 -0400

    tidy

--- rust-playground/made-of-bits/src/lib.rs
+++ rust-playground/made-of-bits/src/lib.rs
@@ -9,11 +9,6 @@ mod zorder;
 
 use to_js::js;
 
-#[js(name_prefix = "foo_")]
-fn add(a: u32, b: u32) -> u32 {
-    a + b
-}
-
 /// Implements a version of `std::panic::catch_unwind` that does not require unwind safety
 /// for its closure argument. This allows us to test the panic behavior of our implementations
 /// without requiring the trait to require `UnwindSafe`. Our testing always clones the `BitVec`
@@ -25,15 +20,4 @@ pub fn catch_unwind<F: FnOnce() -> R, R>(f: F) -> std::thread::Result<R> {
 
 pub fn panics<F: FnOnce() -> R, R>(f: F) -> bool {
     catch_unwind(f).is_err()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = foo_add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
